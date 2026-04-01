649e6eaac31e7c5eb60decf1d1569c27e7d45335
harshitha-parthiban024 <harshipart024@gmail.com>
2026-04-02 00:20:48 +0530

    Create CartContext.js

---
+++ CartContext.js
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+
+  const [cart, setCart] = useState(
+    JSON.parse(localStorage.getItem("cart")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (book) => {
+    setCart([...cart, book]);
+  };
+
+  const removeFromCart = (id) => {
+    setCart(cart.filter(item => item.id !== id));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
