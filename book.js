e1ab07c180534cccb4172508e335e68cae18c6c6
harshitha-parthiban024 <harshipart024@gmail.com>
2026-04-02 00:19:49 +0530

    Add book.js with initial book data

---
+++ book.js
@@ -0,0 +1,18 @@
+const books = [
+  {
+    id: 1,
+    title: "The Midnight Library",
+    author: "Matt Haig",
+    price: 399,
+    image: "/images/ml.png"
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: 450,
+    image: "/images/ah.png"
+  }
+];
+
+export default books;
